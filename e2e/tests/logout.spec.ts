1ca6cda43082c57ed326d66355c17484f2e6a555
Roger Boixader Güell <rboixaderg@gmail.com>
2023-02-19 17:41:53 +0100

    test: logout

--- e2e/tests/logout.spec.ts
+++ e2e/tests/logout.spec.ts
@@ -2,12 +2,13 @@ import { expect, test } from "@playwright/test";
 
 test("Logout", async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  await page.getByRole("link", { name: "Private page" }).click();
+  await page.getByRole("link", { name: "Go to Log in" }).click();
   await page.locator('[data-test="usernameInput"]').click();
   await page.locator('[data-test="usernameInput"]').fill("user_test");
   await page.locator('[data-test="usernameInput"]').press("Tab");
   await page.locator('[data-test="passwordInput"]').fill("user_test");
-  await page.locator('[data-test="passwordInput"]').press("Enter");
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByText("User logged user_test")).toHaveCount(1);
   await page.getByRole("link", { name: "Private page" }).click();
   await page.getByText(/Private Page/);
   await page.getByRole("button", { name: "Logout" }).click();
